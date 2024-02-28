0e05ed98d122415cd44657c8919cda4c506b0983
Jay-Akbari <122269156+jay-simformsolutions@users.noreply.github.com>
2024-02-28 19:15:38 +0530

    👔 Check that factory object auto dispose in get_it (#1)

--- lib/repository/meme_repo.dart
+++ lib/repository/meme_repo.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 class MemeRepo{
   Future<Photo> getMeme() async{
-    final response = await http.get(Uri.parse("https://random.imagecdn.app/v1/image?width=500&height=150&category=buildings&format=json"));
+    final response = await http.get(Uri.parse("https://random.imagecdn.app/v1/image?width=500&height=500&category=buildings&format=json"));
     final data = jsonDecode(response.body);
     return Photo.fromJson(data);
 
